28ff2836133f7e51733b2574107a348f7f597368
Ras3777 <Johnpro3777@gmail.com>
2023-06-13 04:34:11 +0300

    Updated the project schema

--- api/model/project.model.js
+++ api/model/project.model.js
@@ -2,18 +2,17 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const ProjectsSchema = Schema({
-  name: {
+  title: {
     type: String,
     trim: true,
     unique: true,
-    require: "firstname is required",
+    require: "title is required",
   },
   departmentName: {
     type: String,
     trim: true,
-    unique: true,
   },
-  assignedTo: [{ type: String }],
+  assignedTo: { type: String },
   completed: { type: Boolean, default: false },
   created: { type: Date, default: Date.now },
 });
